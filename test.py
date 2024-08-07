fabad032328dc1902afa47cfb042093f01f36d7f
adamscaachi <adam.scaachi.14@ucl.ac.uk>
2024-08-07 13:14:24 +0100

    Implement model testing

---
+++ test.py
@@ -0,0 +1,15 @@
+import argparse
+from ultralytics import YOLO
+
+def test(experiment_name):
+    model_path = f"experiments/{experiment_name}/best.pt"
+    config_path = f"experiments/{experiment_name}/test.yaml"
+    model = YOLO(model_path)
+    metrics = model.val(data=config_path)
+    print(metrics.box.map)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('experiment_name', type=str, help='Name of the experiment.')
+    args = parser.parse_args()
+    test(args.experiment_name)
